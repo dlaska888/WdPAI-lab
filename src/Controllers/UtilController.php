b2f6ad1098f3c2078c799dcf9006e7fd3bce3acb
Dawid Laska <dawidlaska888@gmail.com>
2024-12-26 17:33:24 +0100

    Updated title fetching

--- src/Controllers/UtilController.php
+++ src/Controllers/UtilController.php
@@ -23,14 +23,27 @@ class UtilController extends AppController
         $client = HttpClient::create();
         $url = $_GET['url'];
 
-        $response = $client->request('GET', $url);
+        try {
+            $response = $client->request('GET', $url, [
+                'max_redirects' => 5,
+                'verify_peer' => false,
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+                ]
+            ]);
+        } catch (\Exception $e) {
+            return new Json(["error" => $e->getMessage()]);
+        }
+
         $htmlContent = $response->getContent();
 
         $crawler = new Crawler($htmlContent);
 
         $title = $crawler->filter('title')->text();
-
-        $title = isset($_GET['maxLength']) ? mb_substr($title, 0, $_GET['maxLength']) : $title;
+        $title = mb_convert_encoding($title, 'UTF-8');
+        $title = isset($_GET['maxLength']) ?
+            mb_substr($title, 0, $_GET['maxLength']) :
+            $title;
 
         return new Json(["title" => $title]);
     }
